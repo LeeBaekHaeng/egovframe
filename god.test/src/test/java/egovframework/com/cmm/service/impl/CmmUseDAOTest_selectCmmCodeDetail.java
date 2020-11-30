6ab68a486c5d3edccb056bcc2561b84c5eb535de
이백행 <dlqorgod@naver.com>
2020-12-01 07:56:38 +0900

    GodTestAbstract.java

--- god.test/src/test/java/egovframework/com/cmm/service/impl/CmmUseDAOTest_selectCmmCodeDetail.java
+++ god.test/src/test/java/egovframework/com/cmm/service/impl/CmmUseDAOTest_selectCmmCodeDetail.java
@@ -6,28 +6,19 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import egovframework.com.cmm.ComDefaultCodeVO;
 import egovframework.com.cmm.service.CmmnDetailCode;
+import god.test.GodTestAbstract;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration("classpath*:egovframework/spring/com/**/context-*.xml")
-@ActiveProfiles({ "mysql", "dummy" })
-public class CmmUseDAOTest {
-
-	protected Logger egovLogger = LoggerFactory.getLogger(CmmUseDAOTest.class);
+public class CmmUseDAOTest_selectCmmCodeDetail extends GodTestAbstract {
 
 	@Resource(name = "cmmUseDAO")
 //	@Autowired
 //	@Qualifier("cmmUseDAO")
 	private CmmUseDAO cmmUseDAO;
 
+	@Override
 	@Test
 	public void test() {
 		egovLogger.info("test");
--- god.test/src/test/java/egovframework/com/cmm/service/impl/CmmUseDAOTest_테스트.java
+++ god.test/src/test/java/egovframework/com/cmm/service/impl/CmmUseDAOTest_테스트.java
@@ -6,34 +6,25 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import egovframework.com.cmm.ComDefaultCodeVO;
 import egovframework.com.cmm.service.CmmnDetailCode;
+import god.test.GodTestAbstract;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration("classpath*:egovframework/spring/com/**/context-*.xml")
-@ActiveProfiles({ "mysql", "dummy" })
-public class CmmUseDAOTest {
-
-	protected Logger egovLogger = LoggerFactory.getLogger(CmmUseDAOTest.class);
+public class CmmUseDAOTest_테스트 extends GodTestAbstract {
 
 	@Resource(name = "cmmUseDAO")
 //	@Autowired
 //	@Qualifier("cmmUseDAO")
 	private CmmUseDAO cmmUseDAO;
 
+	@Override
 	@Test
 	public void test() {
 		egovLogger.info("test");
 
 		ComDefaultCodeVO vo = new ComDefaultCodeVO();
-		vo.setCodeId("COM001");
+		vo.setCodeId("COM001-");
 
 		List<CmmnDetailCode> results = selectCmmCodeDetail(vo);
 
